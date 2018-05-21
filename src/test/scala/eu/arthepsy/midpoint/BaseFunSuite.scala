b2cb06c68ffaddee55b3d96819cab3a4cf3de895
Andris Raugulis <moo@arthepsy.eu>
2018-05-21 16:30:20 +0300

    Change year to 2018.

--- src/test/scala/eu/arthepsy/midpoint/BaseFunSuite.scala
+++ src/test/scala/eu/arthepsy/midpoint/BaseFunSuite.scala
@@ -1,6 +1,6 @@
 /*
  * The MIT License
- * Copyright (c) 2017 Andris Raugulis (moo@arthepsy.eu)
+ * Copyright (c) 2018 Andris Raugulis (moo@arthepsy.eu)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
